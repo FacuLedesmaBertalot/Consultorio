e372850946dd6649a3cda6958ca0a7d9a0c54712
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-04-13 12:18:45 -0300

    Método añadido de autenticar medico

--- backend/Controllers/medicoController.js
+++ backend/Controllers/medicoController.js
@@ -37,6 +37,36 @@ const registrar = async (req, res) => {
     }
 };
 
+const autenticar = async (req, res) => {
+    const { email, password } = req.body;
+
+    const usuario = await Medico.findOne({ email });
+
+    if (!usuario) {
+        const error = new Error('El Usuario no Existe');
+        return res.status(404).json({ msg: error.message });
+    }
+
+    if (!usuario.confirmado) {
+        const error = new Error('Tu cuenta no ha sido Confirmada');
+        return res.status(403).json({ msg: error.message });
+    }
+
+    if (await usuario.comprobarPassword(password)) {
+        res.json({
+            _id: usuario._id,
+            nombre: usuario.nombre,
+            email: usuario.email,
+            token: generarJWT(usuario._id)
+        });
+
+    } else {
+        const error = new Error('El Password es incorrecto');
+        return res.status(403).json({ msg: error.message });
+    }
+}
+
 export {
-    registrar
+    registrar,
+    autenticar
 };
