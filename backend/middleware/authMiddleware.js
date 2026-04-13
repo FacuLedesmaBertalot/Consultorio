f95f28934d9fc650372d619ab7151d89844fc937
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-04-13 18:24:43 -0300

    Agregado metodo para que busque la palabra y limpie espacios invisibles

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,29 +1,29 @@
-import jwt from "jsonwebtoken";
-import Medico from "../Models/Medico.js";
+import jwt from 'jsonwebtoken';
+import Medico from '../Models/Medico.js';
 
 const checkAuth = async (req, res, next) => {
     let token;
 
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
         try {
-            token = req.headers.authorization.split('')[1];
+            // MÉTODO A PRUEBA DE BALAS: Reemplazamos la palabra y limpiamos los espacios invisibles
+            token = req.headers.authorization.replace('Bearer', '').trim();
+            
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
             req.medico = await Medico.findById(decoded.id).select("-password -token -confirmado");
-
+            
             return next();
         } catch (error) {
-            const e = new Error('Token no Válido');
+            console.log("Error de JWT:", error.message);
+            const e = new Error('Token no válido');
             return res.status(403).json({ msg: e.message });
         }
     }
 
     if (!token) {
-        const error = new Error('Token Inexistente');
-        res.status(403).json({ msg: error.message});
+        const error = new Error('Token inexistente');
+        res.status(403).json({ msg: error.message });
     }
-
-    next();
 };
 
 export default checkAuth;
