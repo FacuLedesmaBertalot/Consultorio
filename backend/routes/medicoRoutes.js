b44c4182d127b446d65dc214036a8886deea03fb
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-04-13 12:18:32 -0300

    Añadido el router hacia autenticar

--- backend/routes/medicoRoutes.js
+++ backend/routes/medicoRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 import { registrar } from '../Controllers/MedicoController.js';
 
 router.post('/', registrar);
+router.post('/login', autenticar);
 
 export default router;
