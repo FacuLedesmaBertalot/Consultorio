76ed59142db42b5e2cb49235b6f717d28ce9c989
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-04-14 12:45:29 -0300

    Añadido el controlador para el medico, el cual tiene confirmacion por medio de email, y cambio de contraseña

--- backend/Controllers/medicoController.js
+++ backend/Controllers/medicoController.js
@@ -1,5 +1,7 @@
 import Medico from "../Models/Medico.js";
+import generarId from "../helpers/generarId.js";
 import generarJWT from '../helpers/generarJWT.js';
+import { emailRegistro, emailOlvidePassword } from "../helpers/email.js";
 
 const registrar = async (req, res) => {
     const {email, matricula} = req.body;
@@ -22,6 +24,12 @@ const registrar = async (req, res) => {
     try {
         const medico = new Medico(req.body);
         const medicoGuardado = await medico.save();
+
+        emailRegistro({
+            email: medicoGuardado.email,
+            nombre: medicoGuardado.nombre,
+            token: medicoGuardado.token
+        });
 
         res.json({
             msg: 'Registrando Médico...',
@@ -74,8 +82,95 @@ const autenticar = async (req, res) => {
     }
 }
 
+
+const confirmar = async (req, res) => {
+    const { token } = req.params;
+    const usuarioConfirmar = await Medico.findOne({ token });
+
+    if (!usuarioConfirmar) {
+        const error = new Error('Token no válido');
+        return res.status(400).json({ msg: error.message });
+    }
+
+    try {
+        usuarioConfirmar.token = null;
+        usuarioConfirmar.confirmado = true;
+        await usuarioConfirmar.save();
+        res.json({ msg: 'Usuario Confirmado Correctamente'});
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+const olvidePassword = async (req, res) => {
+    const { email } = req.body;
+    const existeMedico = await Medico.findOne({ email });
+
+    if (!existeMedico) {
+        const error = new Error('El Usuario no existe');
+        return res.status(404).json({ msg: error.message });
+    }
+
+    try {
+        existeMedico.token = generarId();
+        await existeMedico.save();
+
+        // Enviar email con instrucciones
+        emailOlvidePassword({
+            email,
+            nombre: existeMedico.nombre,
+            token: existeMedico.token
+        });
+
+        res.json({ msg: 'Hemos enviado un email con las instrucciones' });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const comprobarToken = async (req, res) => {
+    const { token } = req.paramsM
+    const tokenValido = await Medico.findOne({ token });
+
+    if (tokenValido) {
+        res.json({ msg: 'Token Válido y el usuario existe'});
+    } else {
+       const error = new Error('Token no válido');
+       return res.status(400).json({ msg: error.message }); 
+    }
+};
+
+
+const nuevoPassword = async (req, res) => {
+    const { token } = req.params;
+    const { password } = req.body;
+
+    const medico = await Medico.findOne({ token });
+
+    if (!medico) {
+        const error = new Error('Hubo un error');
+        return res.status(400).json({ msg: error.message });
+    }
+
+    try {
+        medico.token = null;
+        medico.password = password; 
+        await medico.save();
+        res.json({ msg: 'Password modificado correctamente' });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+
 export {
     registrar,
     autenticar,
-    perfil
+    perfil,
+    confirmar,
+    olvidePassword,
+    comprobarToken,
+    nuevoPassword
 };
