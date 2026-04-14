eb7bf1cd94b62471c2dbaed22268127f4e31367b
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-04-14 13:31:08 -0300

    Agregado los metodos que faltaban y las rutas modificadas

--- backend/routes/medicoRoutes.js
+++ backend/routes/medicoRoutes.js
@@ -1,13 +1,32 @@
 import express from 'express';
 import checkAuth from '../middleware/authMiddleware.js';
 
-import { registrar, autenticar, perfil } from '../Controllers/MedicoController.js';
+import { 
+    registrar, 
+    autenticar, 
+    perfil, 
+    confirmar, 
+    olvidePassword, 
+    comprobarToken, 
+    nuevoPassword,
+    obtenerMedicosPublicos
+} from '../Controllers/medicoController.js';
 
 
 const router = express.Router();
 
+// Área Pública
 router.post('/', registrar);
 router.post('/login', autenticar);
+router.get('/publicos', obtenerMedicosPublicos);
+
+// Rutas para el manejo de correos y contraseñas
+router.get('/confirmar/:token', confirmar);
+router.post('/olvide-password', olvidePassword);
+
+router.route('/olvide-password/:token')
+    .get(comprobarToken)
+    .post(nuevoPassword);
 
 // Área Privada
 router.get('/perfil', checkAuth, perfil);
