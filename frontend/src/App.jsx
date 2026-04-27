61fb65cbacf6f72843b8048de1107541f25bcf9c
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-04-27 11:57:44 -0300

    Añadido coments para mejor referencia y añadido /admin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,22 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// Layouts
 import MainLayout from './layouts/MainLayout';
+import RutaProtegida from './layouts/RutaProtegida';
+
+// Páginas Públicas
 import Home from './pages/Home';
-import Login from './pages/Login';
-import Registrar from './pages/Registrar';
 import Agendar from './pages/Agendar';
 import Especialistas from './pages/Especialistas';
-import EditarPerfil from './pages/EditarPerfil';
+
+// Páginas de Autenticación
+import Login from './pages/Login';
+import Registrar from './pages/Registrar';
 import ConfirmarCuenta from './pages/ConfirmarCuenta';
+
+// Páginas privadas
+import Admin from './pages/Admin';
+import EditarPerfil from './pages/EditarPerfil';
 
 
 function App() {
@@ -14,17 +24,25 @@ function App() {
     <BrowserRouter>
       <Routes>
         
+        {/* ÁREA PÚBLICA: PACIENTES */}
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Home />} />
           <Route path="agendar" element={<Agendar />} />
           <Route path="especialistas" element={<Especialistas />} />
         </Route>
 
+        {/* ÁREA DE AUTENTICACIÓN: MÉDICOS */}
         <Route path="/login" element={<Login />} />
         <Route path="/registrar" element={<Registrar />} />
-        <Route path="/confirmar/:id" element={<ConfirmarCuenta /> } />
+        <Route path="/confirmar/:id" element={<ConfirmarCuenta />} />
 
-        <Route path="/editar-perfil" element={<EditarPerfil />} />
+        {/* ÁREA PRIVADA: PANEL DE ADMINISTRACIÓN (Protegida por Token) */}
+        <Route path="/admin" element={<RutaProtegida />}>
+
+          <Route index element={<Admin />} />
+          
+          <Route path="perfil" element={<EditarPerfil />} />
+        </Route>
 
       </Routes>
     </BrowserRouter>
