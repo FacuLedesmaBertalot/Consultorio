da644d84118dd9984a5c53c08816a5fe70a20bc1
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-04-27 11:28:13 -0300

    Añadido loginMedicoAPI para chequear si el medico es apto para /admin

--- frontend/src/services/apiMedicos.js
+++ frontend/src/services/apiMedicos.js
@@ -13,3 +13,29 @@ export const registrarMedicoAPI = async datosMedicos => {
         return { ok: false, data: { msg: "Error de conexión" }};
     }
 }
+
+export const loginMedicoAPI = async (credenciales) => {
+    try {
+        const url = `${import.meta.env.VITE_BACKEND_URL}/medicos/login`;
+        const respuesta = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(credenciales)
+        });
+
+        const resultado = await respuesta.json();
+
+        return {
+            ok: respuesta.ok,
+            data: resultado
+        }
+    } catch (error) {
+        console.log("Errir eb loginMedicoAPI:", error);
+        return {
+            ok: false,
+            data: { msg: "Error de conexión con el servidor" }
+        }
+    }
+}
