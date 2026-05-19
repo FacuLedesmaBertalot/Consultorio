32cbdce2a2984edfcd971411aa790068c4b74baa
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-05-19 17:26:21 -0300

    cambios

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,4 @@
 import mongoose from "mongoose";
-import dns from "node:dns"; // 1. Importamos el módulo de red de Node
-
-// 2. Le decimos a Node que use Google solo para este proyecto
-dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 const conectarDB = async () => {
     try {
